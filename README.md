4b9b4b999d20441a6d06232ae3204d24673351b3
xjc10833 <xjc10833@LY.com>
2015-05-25 08:44:26 +0800

    d

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # tc_zt_test
 专题测试自动化
-eee
---
+++ app.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var test = require('./test.json');
+var server = http.createServer(function(req,res){
+	document.header.jQ
+	res.end('console.log("abcdd")')
+});
+server.listen(2000);
